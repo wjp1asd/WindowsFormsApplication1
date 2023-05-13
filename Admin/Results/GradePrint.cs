689b19b51d6d1bc6fad9ec94c9f0e25b675cdb0c
Must Wang <must.wang@buhlergroup.com>
2023-05-13 15:14:33 +0800

    123

---
+++ Admin/Results/GradePrint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.Admin.Results
+{
+    public partial class GradePrint : Form
+    {
+        public GradePrint()
+        {
+            InitializeComponent();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+           // printDocument1.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printDocument1.Print();
+        }
+    }
+}
---
+++ Models/Grade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Models
+{
+   public class Grade
+    {
+        string name;
+        string idcard;
+        string testid;
+        // 离线题目
+        string lxquestions;
+        // 离线回答
+        string lxanswer;
+        // 离线正确性
+        string lxcorrect;
+        string score1;
+        // 在线题
+        string zxquestions;
+        string zxanswer;
+        string zxcorrect;
+        string score2; 
+        //校验题
+        string jyquestions;
+        string jyanswer;
+        string jycorrect;
+        string score3;
+        //型号题
+        string xhquestions;
+        string xhanswer;
+        string xhcorrect;
+        string score4;
+        //零配件
+        string lpjquestions;
+        string lpjanswer;
+        string lpjcorrect;
+        string score5;
+        //研磨膏
+        string ymgquestions;
+        string ymganswer;
+        string ymgcorrect;
+        string score6;
+        //密封面
+        string path;
+        string score7;
+
+    }
+}
