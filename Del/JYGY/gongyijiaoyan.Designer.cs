65d5e5941510bde48c0f6fe35d4fe7ced4cc0655
userjb <user@DESKTOP-48N8H6V>
2023-09-21 10:34:42 +0800

    123

---
+++ Del/JYGY/gongyijiaoyan.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApplication1.Exam
+{
+    partial class gongyijiaoyan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(172, 211);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 56);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "返回";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(172, 123);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(242, 56);
+            this.button3.TabIndex = 41;
+            this.button3.Text = "校验工艺编制";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(142, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(306, 42);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "安全阀模拟考试系统";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // gongyijiaoyan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "gongyijiaoyan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "校验工艺编制";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.gongyijiaoyan_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Del/ON.cs
@@ -0,0 +1,156 @@
+﻿using AutoWindowsSize;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+using WindowsFormsApplication1.Models;
+
+namespace WindowsFormsApplication1.Exam
+{
+    public partial class ON : Form
+    {
+        public ON(String qrcode)
+        {
+            InitializeComponent();
+            InitUI();
+            ff.same(this);
+        }
+        private void InitUI()
+        {
+            datahelp a = new datahelp();
+            a.Initc();
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml(a.color4);
+            this.label2.ForeColor = System.Drawing.ColorTranslator.FromHtml(a.color5);
+            this.button1.BackColor = System.Drawing.ColorTranslator.FromHtml(a.color1);
+
+            this.button2.BackColor = System.Drawing.ColorTranslator.FromHtml(a.color1);
+
+            this.button3.BackColor = System.Drawing.ColorTranslator.FromHtml(a.color1);
+
+
+
+            this.label2.Text = "在线校验";
+
+            ff.same(this);
+         
+            // this.change();
+
+
+        }
+        private Fuc ff = new Fuc();
+        public void change()
+        {
+
+            ff.fullsreen(this.button1, this);
+            ff.fullsreen(this.button2, this);
+            ff.fullsreen(this.button3, this);
+            ff.fullsreen(this.label2, this);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 工艺题'
+            this.Close();
+            gongyijiaoyan gongyijiaoyan = new gongyijiaoyan();
+            gongyijiaoyan.Show();
+
+
+        }
+        AutoAdaptWindowsSize awt;
+        private void groupBox1_Resize(object sender, EventArgs e)
+        {
+
+
+            if (awt != null)
+            {
+
+                awt.FormSizeChanged();
+            }
+        }
+
+
+        protected override CreateParams CreateParams //防止界面闪烁
+        {
+            get
+            {
+                CreateParams paras = base.CreateParams;
+                paras.ExStyle |= 0x02000000;
+                return paras;
+            }
+        }
+
+        private void Choose_Load(object sender, EventArgs e)
+        {
+            awt = new AutoAdaptWindowsSize(this);
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml("white");
+            this.SizeChanged += groupBox1_Resize;
+        }
+        private void ON_Load(object sender, EventArgs e)
+        {
+            awt = new AutoAdaptWindowsSize(this);
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml("white");
+            this.SizeChanged += groupBox1_Resize;
+
+            switch (datahelp.CurrentStep)
+            {
+                case 1:
+
+                    this.button3.Enabled = true;
+                    this.button1.Enabled = false;
+                    this.button2.Enabled = false;
+
+                    break;
+                case 2:
+                    this.button3.Enabled = false;
+                    this.button1.Enabled = true;
+                    this.button2.Enabled = false;
+
+                    break;
+                case 3:
+                    this.button3.Enabled = false;
+                    this.button1.Enabled = false;
+                    this.button2.Enabled = true;
+
+                    break;
+
+
+            }
+            string x = ConfigurationManager.AppSettings["debug"];
+            if (int.Parse(x) == 1)
+            {
+                debug = 1;
+            }
+        }
+        int debug = 0;
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        Grade g= new Grade();
+       // int debug
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //float a = g.getGrade("score4", datahelp.QId);
+            //if (a > -1 && debug != 1)
+            //{
+            //    ff.ShowErrorDialog("重复考试");
+            //    datahelp.CurrentStep = 3;
+            //    return;
+            //}
+                this.Close();
+            datahelp.SubId = 2;
+            datahelp.CurrentStep1 = 1;
+            zaixianjiaoyan a = new zaixianjiaoyan();
+            a.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            ProjectInfo projectInfo = new ProjectInfo();
+            projectInfo.Show();
+        }
+    }
+}
